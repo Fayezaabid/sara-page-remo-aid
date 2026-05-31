bbc95d95db16d41e001b9cdf5265aaf7d4657b22
Fayezaabid <121738422+Fayezaabid@users.noreply.github.com>
2026-05-31 22:11:53 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./index.html', './impressum/*.html', './datenschutz/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Satoshi', 'sans-serif'],
+        display: ['Gabarito', 'sans-serif'],
+        hand: ['Caveat', 'cursive'],
+      },
+      colors: {
+        cream: '#F4EEE0',
+        soft: '#FAF6EC',
+        ink: '#0E0E0E',
+        coral: '#FF6A3D',
+        pink: '#FBD2D8',
+        mint: '#C8EBD3',
+        lilac: '#D9D0F5',
+        butter: '#FFE9A8',
+      },
+    },
+  },
+};
